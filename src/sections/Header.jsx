bb7d936d618347aef10731bb8266736e2d303f53
Vikram-Hegde <vikramhegde1440@gmail.com>
2023-10-29 09:35:05 +0530

    close menu onClick

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -7,7 +7,11 @@ const Header = () => {
 			<div className="logo">
 				Edu<span className="highlight--green">jar</span>
 			</div>
-			<ul role="list" className={`nav ${showMenu ? 'active' : ''}`}>
+			<ul
+				role="list"
+				className={`nav ${showMenu ? 'active' : ''}`}
+				onClick={() => setShowMenu(!showMenu)}
+			>
 				<li>
 					<a href="#home">Home</a>
 				</li>
@@ -30,22 +34,22 @@ const Header = () => {
 			<div className="buttons">
 				<button className="btn btn--outline">Sign Up</button>
 				<div className="icon" onClick={() => setShowMenu(!showMenu)}>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					className="icon icon-tabler icon-tabler-menu"
-					width="24"
-					height="24"
-					viewBox="0 0 24 24"
-					strokeWidth="2"
-					stroke="currentColor"
-					fill="none"
-					strokeLinecap="round"
-					strokeLinejoin="round"
-				>
-					<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-					<path d="M4 8l16 0"></path>
-					<path d="M4 16l16 0"></path>
-				</svg>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						className="icon icon-tabler icon-tabler-menu"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						strokeWidth="2"
+						stroke="currentColor"
+						fill="none"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+					>
+						<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+						<path d="M4 8l16 0"></path>
+						<path d="M4 16l16 0"></path>
+					</svg>
 				</div>
 			</div>
 		</header>
